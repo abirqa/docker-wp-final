fb5612642630884765e76eb77f156fb02ebf95c9
abirqa <abir@theqa.in>
2022-09-01 15:36:39 +0530

    Custom Plugin Dev Strated

---
+++ wp-content/plugins/login page customizer/loginpagecustomizer.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * @package LoginPageCustomizer
+*/
+/*
+
+Plugin Name: Login Page Customizer
+Plugin URI: https://testgithub.com/
+Description: This Plugin will help us to customize the wordpress admin login page from backend by using and font-size, font-family
+or any design patten
+Version: 1.0.0
+Author: QA DEV Team
+Author URI: https://theqa.in/
+License: GPLv2 or Later
+Text Domain: Login Page Customizer by QA DEV Team  
+
+*/
+ 
