5f5ceaf6c87839c101205722410bfdda01fe0831
abirqa <abir@theqa.in>
2022-09-01 16:20:39 +0530

    Done

---
+++ wp-content/plugins/login-page-customizer/login-page-customizer.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * @package LoginPageCustomizer
+ *  
+ */
+/*
+Plugin Name: Login Page Customizer
+Plugin URI: https://testgithub.plugin.com
+Description: Test Description
+Version: 1.0.0
+Author: QA DEV Team
+Author URI: https://theqa.in
+License: GPLv2 or later
+Text Domain: Login-Page-Customizer
+*/
